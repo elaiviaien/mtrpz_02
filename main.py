0f7ed8b7d7d7d823b45211d73dd32be285023d89
elaiviaien <skrypnyk.michael@gmail.com>
2025-04-03 20:12:38 +0300

    Refactor to use built-in list

--- main.py
+++ main.py
@@ -1,142 +1,53 @@
-class ListNode:
-    def __init__(self, data: str):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-
 class DoublyLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self._length = 0
+        self._data = []
 
-    def _get_node_at(self, index: int) -> ListNode:
-        if index < 0 or index >= self._length:
-            raise IndexError("Index out of bounds")
-        current = self.head
-        for _ in range(index):
-            current = current.next
-        return current
-
-    def _delete_node(self, node: ListNode) -> None:
-        if node == self.head:
-            self.head = node.next
-            if self.head:
-                self.head.prev = None
-            else:
-                self.tail = None
-        elif node == self.tail:
-            self.tail = node.prev
-            if self.tail:
-                self.tail.next = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-        self._length -= 1
-    def length(self):
-        return self._length
+    def length(self) -> int:
+        return len(self._data)
 
     def append(self, element: str) -> None:
-        new_node = ListNode(element)
-        if not self.head:
-            self.head = self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        self._length += 1
+        self._data.append(element)
 
     def insert(self, element: str, index: int) -> None:
-        if index < 0 or index > self._length:
+        if index < 0 or index > len(self._data):
             raise IndexError("Index out of bounds")
-
-        if index == self._length:
-            self.append(element)
-        elif index == 0:
-            new_node = ListNode(element)
-            new_node.next = self.head
-            if self.head:
-                self.head.prev = new_node
-            self.head = new_node
-            self._length += 1
-        else:
-            current = self._get_node_at(index)
-            new_node = ListNode(element)
-            new_node.prev = current.prev
-            new_node.next = current
-            current.prev.next = new_node
-            current.prev = new_node
-            self._length += 1
+        self._data.insert(index, element)
 
     def delete(self, index: int) -> str:
-        if index < 0 or index >= self._length:
+        if index < 0 or index >= len(self._data):
             raise IndexError("Index out of bounds")
-
-        node_to_delete = self._get_node_at(index)
-        data = node_to_delete.data
-
-        self._delete_node(node_to_delete)
-        return data
+        return self._data.pop(index)
 
     def delete_all(self, element: str) -> None:
-        current = self.head
-        while current:
-            next_node = current.next
-            if current.data == element:
-                self._delete_node(current)
-            current = next_node
+        self._data = [item for item in self._data if item != element]
 
     def get(self, index: int) -> str:
-        return self._get_node_at(index).data
+        if index < 0 or index >= len(self._data):
+            raise IndexError("Index out of bounds")
+        return self._data[index]
 
     def clone(self) -> "DoublyLinkedList":
         new_list = DoublyLinkedList()
-        current = self.head
-        while current:
-            new_list.append(current.data)
-            current = current.next
+        new_list._data = self._data.copy()
         return new_list
 
     def reverse(self) -> None:
-        current = self.head
-        while current:
-            current.prev, current.next = current.next, current.prev
-            current = current.prev
-        self.head, self.tail = self.tail, self.head
+        self._data.reverse()
 
     def find_first(self, element: str) -> int:
-        current = self.head
-        index = 0
-        while current:
-            if current.data == element:
-                return index
-            current = current.next
-            index += 1
-        return -1
+        try:
+            return self._data.index(element)
+        except ValueError:
+            return -1
 
     def find_last(self, element: str) -> int:
-        current = self.tail
-        index = self._length - 1
-        while current:
-            if current.data == element:
-                return index
-            current = current.prev
-            index -= 1
+        for i in range(len(self._data) - 1, -1, -1):
+            if self._data[i] == element:
+                return i
         return -1
 
     def clear(self) -> None:
-        self.head = self.tail = None
-        self._length = 0
+        self._data.clear()
 
     def extend(self, other: "DoublyLinkedList") -> None:
-        if other.head is None:
-            return
-        if self.head is None:
-            self.head = other.head
-            self.tail = other.tail
-        else:
-            self.tail.next = other.head
-            other.head.prev = self.tail
-            self.tail = other.tail
-        self._length += other._length
+        self._data.extend(other._data)
